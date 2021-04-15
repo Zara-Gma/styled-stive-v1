59b46a355a3e5f9b02ba6601751d91a5cb782ab5
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-15 09:32:35 -0700

    HomePage-folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
 import { Navbar } from "./components";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <GlobalStyle />
       <Navbar />
-    </div>
+      <Switch></Switch>
+    </Router>
   );
 }
 
